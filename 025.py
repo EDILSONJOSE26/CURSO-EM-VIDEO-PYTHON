4467c3ef60ccbfacc8ee14d8b21cf8854653d86e
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2022-10-08 14:22:44 -0300

    atualização apl

--- 025.py
+++ 025.py
@@ -1,4 +1,4 @@
 '''crie um programa que leia o nome de uma pessoa e diga se ela tem ''silva'' no nome'''
 
-nome = str(input("Digite seu nome completo: ").lower())
-print('silva' in nome)
+nome = str(input("Digite seu nome completo: ")).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
