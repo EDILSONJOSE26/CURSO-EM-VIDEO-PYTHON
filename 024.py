0f70a754d9f7c3c7bbf549cf72e6b7a3b26d4f38
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2022-10-08 13:50:23 -0300

    up

---
+++ 024.py
@@ -0,0 +1,5 @@
+'''crien um programa que leia o nome de uma cidade e diga se ela começa com o nome ''Santos''. '''
+
+
+c = str(input('Digite o nome da sua cidade: '))
+print(c.title().find('Santos'))
