7faf40e53cc354866953260ec26e240b2f2b6fac
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2022-10-08 14:15:14 -0300

    atualização ap

--- 024.py
+++ 024.py
@@ -1,5 +1,5 @@
 '''crien um programa que leia o nome de uma cidade e diga se ela começa com o nome ''Santos''. '''
 
 
-c = str(input('Digite o nome da sua cidade: '))
-print(c.title().find('Santos'))
+c = str(input('Digite o nome da sua cidade: ')).strip()
+print(c.upper() == 'SANTOS')
