4ac8e111fe1171254c40ef433b533a49a15e7683
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2022-10-08 14:08:10 -0300

    atualização c

--- 023.py
+++ 023.py
@@ -1,7 +1,7 @@
 '''faça um programa que leia um número de 0 a 9999 e mostre na tela 
 cada um dos digitos separados.'''
 
-num = int(input("Digite um numero até 9999: ").strip())
+num = int(input("Digite um numero até 9999: "))
 u = num // 1 % 10
 d = num // 10 % 10
 c = num // 100 % 10
