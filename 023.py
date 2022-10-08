26aaf13b6a4544f66da4a49c398dd363d99d573f
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2022-10-08 14:05:51 -0300

    atualização c

--- 023.py
+++ 023.py
@@ -1,19 +1,14 @@
 '''faça um programa que leia um número de 0 a 9999 e mostre na tela 
 cada um dos digitos separados.'''
 
-#S failed 90%
-
 num = int(input("Digite um numero até 9999: ").strip())
-uni = num % 10
-dez = num % 10
-s_centena = (num - dez) / 10
-centena = s_centena
-milhar = 
-
-
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
 print(""" 
 Unidade = {}
 Dezena = {}
 Centena = {}
 Milhar = {}
-""".format(uni,dez,centena,milhar))
+""".format(u,d,c,m))
