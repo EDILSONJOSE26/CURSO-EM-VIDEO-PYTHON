4733fec599ec99b73252a61be7dd21fabb1bc02e
EDILSONJOSE26 <101295282+EDILSONJOSE26@users.noreply.github.com>
2022-10-12 19:25:36 -0300

    corrigido

--- 039.py
+++ 039.py
@@ -3,13 +3,16 @@
 - se é a hora de se alistar .
 - se já passou do tempo do alistamento'''
 
+from datetime import date
+atual = date.today().year
 ano_ncs = int(input('Digite seu ano de nascimento: '))
-
-soma_a = (2022 - ano_ncs)
-
-if soma_a < 18:
-    print('idade {} ainda vai se alistar'.format(soma_a))
-elif soma_a == 18:
-    print('idade {} é a hora de se alistar'.format(soma_a))
-elif soma_a > 18:
-    print('idade {} já passou do tempo'.format(soma_a))
+idade = (atual - ano_ncs)
+print('Quem nasceu em {} tem {} anos em {}'.format(ano_ncs, idade, atual))
+if idade < 18:
+    saldo = 18 -idade
+    print('ainda faltam {} anos para o alistamento'.format(idade, saldo))
+elif idade == 18:
+    print('Voçê tem que se alistar IMEDIATAMENTE!')
+elif idade > 18:
+    saldo = idade - 18
+    print('Voçê ja deveria ter se alistado há {} anos'.format(saldo))
